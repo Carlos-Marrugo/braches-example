6e7b7ae266619e68fe6e6155f4f9a57855035970
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2024-09-26 13:57:35 -0500

    cambio desde login

--- register.java
+++ register.java
@@ -4,5 +4,7 @@ public class register {
         System.out.println("Esto lo subi desde la rama2");
         System.out.println("lo nuevo");
 
+        System.out.println("Nuevo cambio a master desde login");
+
     }
 }
